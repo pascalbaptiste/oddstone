6ff9b154761fbcada2e92c1eccf79ef89526f3f6
J.L. Makes <julianlloyd@gmail.com>
2016-05-25 07:59:36 +0200

    repo(*): :sparkles: initial commit :sparkles:

---
+++ Assets/Scripts/Controller2D.cs
@@ -0,0 +1,162 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent (typeof (BoxCollider2D))]
+public class Controller2D : MonoBehaviour {
+
+	// When our character is flush with the ground, a small
+	// offset is used to reduce the collider bounds, so that
+	// we have some space for raycasting.
+	const float boundsOffset = 0.015f;
+
+	public LayerMask collisionMask;
+
+	public int horizontalRayCount = 4;
+	public int verticalRayCount = 4;
+
+	float horizontalRaySpacing;
+	float verticalRaySpacing;
+
+	new BoxCollider2D collider;
+	RaycastOrigins raycastOrigins;
+
+	public CollisionInfo collisions;
+	public JumpInfo jumpInfo;
+
+	void Start () {
+		collider = GetComponent<BoxCollider2D> ();
+		CalculateRaySpacing ();
+
+		// Confirm our controller is registered.
+		if (Input.GetJoystickNames ().Length > 0) {
+			print ("DualShock 4 Ready!");
+		}
+	}
+
+	public void Move (Vector3 velocity) {
+		UpdateRaycastOrigins ();
+
+		// Delete the previous state, and check for collisions.
+		collisions.Reset ();
+		if (velocity.x != 0) CheckHorizontalCollisions (ref velocity);
+		if (velocity.y != 0) CheckVerticalCollisions (ref velocity);
+
+		transform.Translate (velocity);
+	}
+
+	void CheckHorizontalCollisions (ref Vector3 velocity) {
+		// Resolves to 1 for right movement, or -1 for left movement.
+		float directionX = Mathf.Sign (velocity.x);
+
+		// We need to add the bounds offset back for an accurate ray length.
+		float rayLength = Mathf.Abs (velocity.x) + boundsOffset;
+
+		for (int i = 0; i < horizontalRayCount; i++) {
+			// We’ll start our raycasting from the bottom left bound when moving
+			// left, and from the bottom right bound when moving right.
+			Vector2 rayOrigin = (directionX == -1) ? raycastOrigins.bottomLeft : raycastOrigins.bottomRight;
+
+			// Our ray origin will be offset right by the ray spacing, but we also
+			// want to make sure to include the horizontal velocity so that rays are
+			// cast from the imminent horizontal position (not where we used to be).
+			rayOrigin += Vector2.up * (horizontalRaySpacing * i);
+			
+			// Visualize ray casting...
+			Debug.DrawRay(rayOrigin, Vector2.right * directionX * rayLength, Color.white);
+
+			RaycastHit2D hit = Physics2D.Raycast (rayOrigin, Vector2.right * directionX, rayLength, collisionMask);
+			if (hit) {
+				// Velocity matches distance to collision objection,
+				// so when distance is 0, velocity is 0.
+				velocity.x = (hit.distance - boundsOffset) * directionX;
+				rayLength = hit.distance;
+
+				// Update our collisions struct.
+				collisions.left = directionX == -1;
+				collisions.right = directionX == 1;
+			}
+		}
+	}
+
+	void CheckVerticalCollisions (ref Vector3 velocity) {
+		// Resolves to 1 for upward movement, or -1 for downward movement.
+		float directionY = Mathf.Sign (velocity.y);
+
+		// We need to add the bounds offset back for an accurate ray length.
+		float rayLength = Mathf.Abs (velocity.y) + boundsOffset;
+
+		for (int i = 0; i < verticalRayCount; i++) {
+			// We’ll start our raycasting from the bottom left bound when moving
+			// downward, and from the top left bound when moving upwards.
+			Vector2 rayOrigin = (directionY == -1) ? raycastOrigins.bottomLeft : raycastOrigins.topLeft;
+
+			// Our ray origin will be offset right by the ray spacing, but we also
+			// want to make sure to include the horizontal velocity so that rays are
+			// cast from the imminent horizontal position (not where we used to be).
+			rayOrigin += Vector2.right * (verticalRaySpacing * i + velocity.x);
+			
+			// Visualize ray casting...
+			Debug.DrawRay(rayOrigin, Vector2.up * directionY * rayLength, Color.white);
+
+			RaycastHit2D hit = Physics2D.Raycast (rayOrigin, Vector2.up * directionY, rayLength, collisionMask);
+			if (hit) {
+				// Velocity matches distance to collision objection,
+				// so when distance is 0, velocity is 0.
+				velocity.y = (hit.distance - boundsOffset) * directionY;
+				rayLength = hit.distance;
+
+				// Update our collisions struct.
+				collisions.below = directionY == -1;
+				collisions.above = directionY == 1;
+			}
+		}
+	}
+
+	/**
+	 * Simple method that returns the collider bounds,
+	 * but slightly shrunken using our bounds offset.
+	 **/
+	Bounds getBounds () {
+		Bounds bounds = collider.bounds;
+		bounds.Expand (boundsOffset * -2);
+		return bounds;
+	}
+		
+	void UpdateRaycastOrigins () {
+		Bounds bounds = getBounds ();
+		raycastOrigins.topLeft = new Vector2 (bounds.min.x, bounds.max.y);
+		raycastOrigins.topRight = new Vector2 (bounds.max.x, bounds.max.y);
+		raycastOrigins.bottomLeft = new Vector2 (bounds.min.x, bounds.min.y);
+		raycastOrigins.bottomRight = new Vector2 (bounds.max.x, bounds.min.y);
+	}
+
+	void CalculateRaySpacing () {
+		Bounds bounds = getBounds ();
+
+		// Ensure that we have at least 2 rays per axis.
+		horizontalRayCount = Mathf.Clamp (horizontalRayCount, 2, int.MaxValue);
+		verticalRayCount = Mathf.Clamp (verticalRayCount, 2, int.MaxValue);
+
+		// Calculate the appropriate space between rays.
+		horizontalRaySpacing = bounds.size.y / (horizontalRayCount - 1);
+		verticalRaySpacing = bounds.size.x / (verticalRayCount - 1);
+	}
+
+	struct RaycastOrigins {
+		public Vector2 topLeft, topRight;
+		public Vector2 bottomLeft, bottomRight;
+	}
+
+	public struct CollisionInfo {
+		public bool above, below;
+		public bool left, right;
+		public void Reset () {
+			above = below = false;
+			left = right = false;
+		}
+	}
+
+	public struct JumpInfo {
+		public int count;
+	}
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,71 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent (typeof (Controller2D))]
+public class Player : MonoBehaviour {
+
+	float accelerationTimeAir = 0.2f;
+	float accelerationTimeGround = 0.1f;
+	float timeToJumpApex = 0.4f;
+	float jumpHeight = 3f;
+	float moveSpeed = 6f;
+
+	float gravity;
+	float jumpVelocity;
+	float velocityXSmoothing;
+	Vector3 velocity;
+
+	Controller2D controller;
+
+	void Start () {
+		controller = GetComponent<Controller2D> ();
+
+		gravity = -(2 * jumpHeight / Mathf.Pow(timeToJumpApex, 2));
+		jumpVelocity = Mathf.Abs (gravity) * timeToJumpApex;
+	}
+
+	void Update () {
+		// capture device input
+		float horizontal = Input.GetAxisRaw ("Horizontal");
+		float vertical = Input.GetAxisRaw ("Vertical");
+		Vector2 input = new Vector2 (horizontal, vertical);
+
+		// Store reference to jump button.
+		bool jumpButton = Input.GetKeyDown (KeyCode.JoystickButton1);
+
+		// If we jump into something, nuke all vertical velocity.
+		if (controller.collisions.above) {
+			velocity.y = 0;
+		}
+		// Or if we are on the ground, nuke all vertical velocity
+		// and reset our jump counter.
+		else if (controller.collisions.below) {
+			velocity.y = 0;
+			controller.jumpInfo.count = 0;
+		}
+		// Otherwise we must be in freefall, so let’s update our
+		// jump count to only allow for a single jump
+		else if (controller.jumpInfo.count != 2) {
+			controller.jumpInfo.count = 1;
+		}
+
+		// If our jump button is pressed, and we haven’t reached
+		// our double jump limit, let’s update our vertical velocity.
+		if (jumpButton && controller.jumpInfo.count < 2) {
+			velocity.y = jumpVelocity;
+			controller.jumpInfo.count++;
+		}
+
+		// Augment our horizontal velocity using a `Smooth Damp` function to
+		// emulate acceleration, using different values for air and ground travel.
+		float targetVelocityX = input.x * moveSpeed;
+		float smoothTime = (controller.collisions.below) ? accelerationTimeGround : accelerationTimeAir;
+		velocity.x = Mathf.SmoothDamp (velocity.x, targetVelocityX, ref velocityXSmoothing, smoothTime);
+
+		// Augment our vertical velocity with gravity.
+		velocity.y += gravity * Time.deltaTime;
+
+		// Send the updated velocity to our controller!
+		controller.Move (velocity * Time.deltaTime);
+	}
+}
